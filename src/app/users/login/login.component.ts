10d2f9a0ec1e3a595fc4a9a01162e2eb21af9a06
Van Salido <vansalido@gmail.com>
2023-07-13 10:50:21 +0800

    updated login and registration

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -5,6 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker'
 import {MatNativeDateModule} from '@angular/material/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
@@ -16,7 +17,9 @@ import {MatNativeDateModule} from '@angular/material/core';
     MatIconModule,
     MatInputModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+
+    ReactiveFormsModule
 
   ],
   templateUrl: './login.component.html',
@@ -25,7 +28,29 @@ import {MatNativeDateModule} from '@angular/material/core';
 export class LoginComponent {
   @Output() switchEvent = new EventEmitter<string>();
 
-  toRegistrationMode(){
+  loginUserControlGroup = new FormGroup({
+    userName: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required])
+  })
+
+  isErrorState = false;
+
+  getAccessToken(userName:string|null, password:string|null): string|null{
+    // Todo
+    return 'Bearer aCCessTokenSample123XyZ!@#'
+  }
+
+  onSubmit():void {
+    let userName = this.loginUserControlGroup.controls['userName'].value
+    let password = this.loginUserControlGroup.controls['password'].value
+    let accessToken = this.getAccessToken(userName,password)
+
+    //Todo accessToken
+    this.isErrorState = true;
+    console.log(userName,password, accessToken)
+  }
+
+  toRegistrationMode():void{
     this.switchEvent.emit('registration')
   }
 }
--- src/app/users/register/register.component.ts
+++ src/app/users/register/register.component.ts
@@ -4,13 +4,26 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { ErrorStateMatcher, MatNativeDateModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
-import { FormControl,FormGroupDirective,FormsModule,NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl,FormGroupDirective,FormsModule,NgForm, ReactiveFormsModule, Validators, FormGroup, AbstractControl } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 export class PasswordMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-      return true
+      const isPasswordMatched = form?.control.controls['password'].value === control?.value
+      const isPasswordAndConfirmationFilled = form?.control.controls['password'].value&&control?.value
+      return isPasswordAndConfirmationFilled&&!isPasswordMatched
+  }
+}
+
+export class UsernameMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+      const fieldValue = control?.value
+      return fieldValue && this.isUsernameExists(fieldValue)
+  }
+  isUsernameExists(userName:string|null):boolean{
+    //  Todo
+    return false
   }
 }
 
@@ -39,6 +52,24 @@ export class RegisterComponent {
     this.switchEvent.emit('login')
   }
 
+  usernameMatcher = new UsernameMatcher();
   passwordMatcher = new PasswordMatcher();
-  registrationFormControl = new FormControl('', [Validators.required])
+
+  registrationFormControlGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+    fullName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    birthDate: new FormControl('', [Validators.required]),
+  })
+
+  onSubmit(){
+    if(this.registrationFormControlGroup.valid){
+      //Todo API Call
+    }
+
+  }
+
+
 }
